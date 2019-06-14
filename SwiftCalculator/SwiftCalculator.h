9804e56cde5e770b38209a9bf591231b3e23ea5b
Adobels <contact@blazejsleboda.fr>
2019-06-14 09:26:11 +0200

    Initial Commit

---
+++ SwiftCalculator/SwiftCalculator.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCalculator.h
+//  SwiftCalculator
+//
+//  Created by Blazej SLEBODA on 14/06/2019.
+//  Copyright © 2019 Blazej SLEBODA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCalculator.
+FOUNDATION_EXPORT double SwiftCalculatorVersionNumber;
+
+//! Project version string for SwiftCalculator.
+FOUNDATION_EXPORT const unsigned char SwiftCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCalculator/PublicHeader.h>
+
+
